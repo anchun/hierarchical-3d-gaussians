724af74864255fdd4c3ab33cd02f4ce326e0beba
caihong <caihong@51hitech.com>
2025-02-18 15:07:18 +0800

    up

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -254,3 +254,24 @@ def quaternion_to_matrix(r):
     R[:, 2, 2] = 1 - 2 * (x*x + y*y)
     return R
 
+
+def quaternion_to_matrix_numpy(r):
+    q = r / np.linalg.norm(r)
+
+    R = np.zeros((3, 3))
+
+    r = q[0]
+    x = q[1]
+    y = q[2]
+    z = q[3]
+
+    R[0, 0] = 1 - 2 * (y*y + z*z)
+    R[0, 1] = 2 * (x*y - r*z)
+    R[0, 2] = 2 * (x*z + r*y)
+    R[1, 0] = 2 * (x*y + r*z)
+    R[1, 1] = 1 - 2 * (x*x + z*z)
+    R[1, 2] = 2 * (y*z - r*x)
+    R[2, 0] = 2 * (x*z - r*y)
+    R[2, 1] = 2 * (y*z + r*x)
+    R[2, 2] = 1 - 2 * (x*x + y*y)
+    return R
